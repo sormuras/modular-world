84ff526d5343f211997cbb09bdcc51f37e7d1936
Christian Stein <sormuras@gmail.com>
2019-05-09 13:23:24 +0200

    Add 011-mr-abc project

---
+++ 011-mr-abc/src/main/b/b/B.java
@@ -0,0 +1,7 @@
+package b;
+
+import a.A;
+
+public class B {
+  public A a = new A();
+}
---
+++ 011-mr-abc/src/main/c/c/Main.java
@@ -0,0 +1,10 @@
+package c;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("  a -> " + new a.A());
+    System.out.println("  b -> " + new b.B());
+    System.out.println("  c -> " + new C());
+  }
+}
